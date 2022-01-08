cca7f9d2be9cfd4852a466508aa8fda383d1a04f
Igor Belyi <belyi@ac93.org>
2022-01-07 20:10:03 -0500

    day02 solution

---
+++ src/day02/mod.rs
@@ -0,0 +1,57 @@
+use super::common::Step;
+
+pub fn test_result(step: &Step) -> String {
+    match step {
+        Step::First => 30.to_string(),
+        Step::Second => 0.to_string(),
+    }
+}
+
+fn count(data: &[i32]) -> i32 {
+    for noun in 0..100 {
+        for verb in 0..100 {
+            if run(data, noun, verb) == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    0
+}
+
+fn run(data: &[i32], noun: i32, verb: i32) -> i32 {
+    let mut opcode: Vec<i32> = data.to_owned();
+    opcode[1] = noun;
+    opcode[2] = verb;
+    let mut i = 0;
+    while i < opcode.len() {
+        if opcode[i] == 99 {
+            break;
+        }
+        let idx1 = opcode[i + 1] as usize;
+        let idx2 = opcode[i + 2] as usize;
+        let idx3 = opcode[i + 3] as usize;
+        if idx1 >= opcode.len() || idx2 >= opcode.len() || idx3 >= opcode.len() {
+            return 0;
+        }
+        opcode[idx3] = if opcode[i] == 1 {
+            opcode[idx1] + opcode[idx2]
+        } else if opcode[i] == 2 {
+            opcode[idx1] * opcode[idx2]
+        } else {
+            return 0;
+        };
+        i += 4;
+    }
+    opcode[0]
+}
+
+pub fn solution(step: &Step, input: &[String]) -> String {
+    let data: Vec<i32> = input[0]
+        .split(',')
+        .map(|v| v.parse().expect("Not a number!"))
+        .collect();
+    match step {
+        Step::First => run(&data, 12, 2).to_string(),
+        Step::Second => count(&data).to_string(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use aoc2019::{
     common::{lines_from_file, Step},
-    day01::{solution, test_result},
+    day02::{solution, test_result},
 };
-const DAY: &str = "day01";
+const DAY: &str = "day02";
 
 fn main() {
     let test_input = String::from("./src/") + DAY + "/test_input.txt";
